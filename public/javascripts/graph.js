a42172dc1b7a0b466a50d21168dafeed86acab49
seowyanyi <seow.yanyi@gmail.com>
2015-09-08 21:15:42 +0800

    generate multiple graphs

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -17,97 +17,102 @@ function getUrlVars() {
 $(window).bind("load", function() {
     var transId = getUrlVars().transaction_id;
     $.get('http://localhost:3000/map?transaction_id='+transId, function(data) {
-        var edgeData = data.graph.edges;
-        var nodeData = data.graph.nodes;
-        console.log(data);
+        var graphData = data.graph;
         var RADIUS = 15;
+        for (var x = 0; x < graphData.length; ++x) {
 
-        //todo: update data for each stage
+            var edgeData = graphData[x].edges;
+            var nodeData = graphData[x].nodes;
+            var stageNum = graphData[x].stage;
 
-        var xScale = d3.scale.linear()
-            .domain([d3.min(edgeData, function (d) {
-                return d.start[0];
-            }),
-                d3.max(edgeData, function (d) {
+
+            var xScale = d3.scale.linear()
+                .domain([d3.min(edgeData, function (d) {
                     return d.start[0];
-                })])
-            .range([50, w - 100]);
+                }),
+                    d3.max(edgeData, function (d) {
+                        return d.start[0];
+                    })])
+                .range([50, w - 100]);
 
-        var yScale = d3.scale.linear()
-            .domain([d3.min(edgeData, function (d) {
-                return d.start[1];
-            }),
-                d3.max(edgeData, function (d) {
+            var yScale = d3.scale.linear()
+                .domain([d3.min(edgeData, function (d) {
+                    return d.start[1];
+                }),
+                    d3.max(edgeData, function (d) {
+                        return d.start[1];
+                    })])
+                .range([50, h - 100]);
+
+            var rScale = d3.scale.linear()
+                .domain([0, d3.max(edgeData, function (d) {
                     return d.start[1];
                 })])
-            .range([50, h - 100]);
+                .range([14, 17]);
+            console.log('stage num: ' + stageNum);
+            var svg = d3.select(".stage" + stageNum).append("svg")
+                .attr({"width": w, "height": h})
+                .style("border", "1px solid black");
 
-        var rScale = d3.scale.linear()
-            .domain([0, d3.max(edgeData, function (d) {
-                return d.start[1];
-            })])
-            .range([14, 17]);
+            var elemEdge = svg.selectAll("line")
+                .data(edgeData)
+                .enter();
 
-        var svg = d3.select(".stage" + data.graph.stage).append("svg")
-            .attr({"width": w, "height": h})
-            .style("border", "1px solid black");
+            var edges = elemEdge.append("line")
+                .attr("x1", function (d) {
+                    return xScale(d.start[0]);
+                })
+                .attr("y1", function (d) {
+                    return yScale(d.start[1]);
+                })
+                .attr("x2", function (d) {
+                    return xScale(d.end[0]);
+                })
+                .attr("y2", function (d) {
+                    return yScale(d.end[1]);
+                })
+                .attr("stroke-width", 2)
+                .attr("stroke", "black");
 
-        var elemEdge = svg.selectAll("line")
-            .data(edgeData)
-            .enter();
 
-        var edges = elemEdge.append("line")
-            .attr("x1", function (d) {
-                return xScale(d.start[0]);
-            })
-            .attr("y1", function (d) {
-                return yScale(d.start[1]);
-            })
-            .attr("x2", function (d) {
-                return xScale(d.end[0]);
-            })
-            .attr("y2", function (d) {
-                return yScale(d.end[1]);
-            })
-            .attr("stroke-width", 2)
-            .attr("stroke", "black");
+            var elemNode = svg.selectAll("circle")
+                .data(nodeData)
+                .enter();
 
-        var elemNode = svg.selectAll("circle")
-            .data(nodeData)
-            .enter();
+            var nodes = elemNode.append("circle")
+                .attr("cx", function (d) {
+                    return xScale(parseInt(d.x));
+                })
+                .attr("cy", function (d) {
+                    return yScale(parseInt(d.y));
+                })
+                .attr({"r": rScale(RADIUS)})
+                .style("fill", "yellow")
+                .style("stroke", "black");
 
-        var nodes = elemNode.append("circle")
-            .attr("cx", function (d) {
-                return xScale(parseInt(d.x));
-            })
-            .attr("cy", function (d) {
-                return yScale(parseInt(d.y));
-            })
-            .attr({"r": rScale(RADIUS)})
-            .style("fill", "yellow")
-            .style("stroke", "black");
+            var nodeNames = elemNode.append("text")
+                .attr("x", function (d) {
+                    return xScale(parseInt(d.x)) + 5;
+                })
+                .attr("y", function (d) {
+                    return yScale(parseInt(d.y)) - 15;
+                })
+                .text(function (d) {
+                    return d.nodeName;
+                });
 
-        var nodeNames = elemNode.append("text")
-            .attr("x", function (d) {
-                return xScale(parseInt(d.x)) + 5;
-            })
-            .attr("y", function (d) {
-                return yScale(parseInt(d.y)) - 15;
-            })
-            .text(function (d) {
-                return d.nodeName;
-            });
+            var nodeId = elemNode.append("text")
+                .attr("x", function (d) {
+                    return xScale(parseInt(d.x)) - rScale(RADIUS) / 2;
+                })
+                .attr("y", function (d) {
+                    return yScale(parseInt(d.y)) + rScale(RADIUS) / 2;
+                })
+                .text(function (d) {
+                    return d.nodeId;
+                });
+        }
 
-        var nodeId = elemNode.append("text")
-            .attr("x", function (d) {
-                return xScale(parseInt(d.x)) - rScale(RADIUS) / 2;
-            })
-            .attr("y", function (d) {
-                return yScale(parseInt(d.y)) + rScale(RADIUS) / 2;
-            })
-            .text(function (d) {
-                return d.nodeId;
-            });
     });
 
 });
